f8f3bdc83a241290769efb8a1d3f50c9541586fe
jose noriega <thejose123654@hotmail.es>
2018-03-02 18:14:37 -0600

    init

---
+++ PlantillTallerArduino/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace PlantillTallerArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb_Door = new System.Windows.Forms.PictureBox();
+            this.btnOpenDoor = new System.Windows.Forms.Button();
+            this.btnCloseDoor = new System.Windows.Forms.Button();
+            this.btnOpenG = new System.Windows.Forms.Button();
+            this.btnCloseG = new System.Windows.Forms.Button();
+            this.pb_Garage = new System.Windows.Forms.PictureBox();
+            this.light_5 = new System.Windows.Forms.PictureBox();
+            this.light_7 = new System.Windows.Forms.PictureBox();
+            this.light_6 = new System.Windows.Forms.PictureBox();
+            this.light_4 = new System.Windows.Forms.PictureBox();
+            this.light_1 = new System.Windows.Forms.PictureBox();
+            this.light_2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.light_3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Door)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Garage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_Door
+            // 
+            this.pb_Door.BackColor = System.Drawing.Color.Transparent;
+            this.pb_Door.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pb_Door.Location = new System.Drawing.Point(304, 357);
+            this.pb_Door.Name = "pb_Door";
+            this.pb_Door.Size = new System.Drawing.Size(42, 100);
+            this.pb_Door.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Door.TabIndex = 1;
+            this.pb_Door.TabStop = false;
+            // 
+            // btnOpenDoor
+            // 
+            this.btnOpenDoor.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenDoor.Name = "btnOpenDoor";
+            this.btnOpenDoor.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenDoor.TabIndex = 2;
+            this.btnOpenDoor.Text = "Open D";
+            this.btnOpenDoor.UseVisualStyleBackColor = true;
+            this.btnOpenDoor.Click += new System.EventHandler(this.btnOpenDoor_Click);
+            // 
+            // btnCloseDoor
+            // 
+            this.btnCloseDoor.Location = new System.Drawing.Point(106, 11);
+            this.btnCloseDoor.Name = "btnCloseDoor";
+            this.btnCloseDoor.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseDoor.TabIndex = 3;
+            this.btnCloseDoor.Text = "Close D";
+            this.btnCloseDoor.UseVisualStyleBackColor = true;
+            this.btnCloseDoor.Click += new System.EventHandler(this.btnCloseDoor_Click);
+            // 
+            // btnOpenG
+            // 
+            this.btnOpenG.Location = new System.Drawing.Point(428, 12);
+            this.btnOpenG.Name = "btnOpenG";
+            this.btnOpenG.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenG.TabIndex = 4;
+            this.btnOpenG.Text = "Open G";
+            this.btnOpenG.UseVisualStyleBackColor = true;
+            this.btnOpenG.Click += new System.EventHandler(this.btnOpenG_Click);
+            // 
+            // btnCloseG
+            // 
+            this.btnCloseG.Location = new System.Drawing.Point(525, 12);
+            this.btnCloseG.Name = "btnCloseG";
+            this.btnCloseG.Size = new System.Drawing.Size(75, 23);
+            this.btnCloseG.TabIndex = 5;
+            this.btnCloseG.Text = "Close G";
+            this.btnCloseG.UseVisualStyleBackColor = true;
+            this.btnCloseG.Click += new System.EventHandler(this.btnCloseG_Click);
+            // 
+            // pb_Garage
+            // 
+            this.pb_Garage.BackColor = System.Drawing.Color.Transparent;
+            this.pb_Garage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_Garage.Location = new System.Drawing.Point(497, 345);
+            this.pb_Garage.Name = "pb_Garage";
+            this.pb_Garage.Size = new System.Drawing.Size(275, 109);
+            this.pb_Garage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_Garage.TabIndex = 6;
+            this.pb_Garage.TabStop = false;
+            // 
+            // light_5
+            // 
+            this.light_5.BackColor = System.Drawing.Color.Transparent;
+            this.light_5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light_5.BackgroundImage")));
+            this.light_5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_5.Location = new System.Drawing.Point(110, 107);
+            this.light_5.Name = "light_5";
+            this.light_5.Size = new System.Drawing.Size(80, 40);
+            this.light_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_5.TabIndex = 7;
+            this.light_5.TabStop = false;
+            // 
+            // light_7
+            // 
+            this.light_7.BackColor = System.Drawing.Color.Transparent;
+            this.light_7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light_7.BackgroundImage")));
+            this.light_7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_7.Location = new System.Drawing.Point(266, 82);
+            this.light_7.Name = "light_7";
+            this.light_7.Size = new System.Drawing.Size(80, 40);
+            this.light_7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_7.TabIndex = 8;
+            this.light_7.TabStop = false;
+            // 
+            // light_6
+            // 
+            this.light_6.BackColor = System.Drawing.Color.Transparent;
+            this.light_6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light_6.BackgroundImage")));
+            this.light_6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_6.Location = new System.Drawing.Point(300, 140);
+            this.light_6.Name = "light_6";
+            this.light_6.Size = new System.Drawing.Size(90, 50);
+            this.light_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_6.TabIndex = 9;
+            this.light_6.TabStop = false;
+            this.light_6.Click += new System.EventHandler(this.light_6_Click);
+            // 
+            // light_4
+            // 
+            this.light_4.BackColor = System.Drawing.Color.Transparent;
+            this.light_4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light_4.BackgroundImage")));
+            this.light_4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_4.Location = new System.Drawing.Point(285, 281);
+            this.light_4.Name = "light_4";
+            this.light_4.Size = new System.Drawing.Size(100, 60);
+            this.light_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_4.TabIndex = 11;
+            this.light_4.TabStop = false;
+            // 
+            // light_1
+            // 
+            this.light_1.BackColor = System.Drawing.Color.Transparent;
+            this.light_1.BackgroundImage = global::PlantillTallerArduino.Properties.Resources.lightOff;
+            this.light_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_1.Location = new System.Drawing.Point(35, 389);
+            this.light_1.Name = "light_1";
+            this.light_1.Size = new System.Drawing.Size(90, 60);
+            this.light_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_1.TabIndex = 12;
+            this.light_1.TabStop = false;
+            // 
+            // light_2
+            // 
+            this.light_2.BackColor = System.Drawing.Color.Transparent;
+            this.light_2.BackgroundImage = global::PlantillTallerArduino.Properties.Resources.lightOff;
+            this.light_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_2.Location = new System.Drawing.Point(145, 389);
+            this.light_2.Name = "light_2";
+            this.light_2.Size = new System.Drawing.Size(90, 60);
+            this.light_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_2.TabIndex = 13;
+            this.light_2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Turn on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Turn off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // light_3
+            // 
+            this.light_3.BackColor = System.Drawing.Color.Transparent;
+            this.light_3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("light_3.BackgroundImage")));
+            this.light_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.light_3.Location = new System.Drawing.Point(81, 281);
+            this.light_3.Name = "light_3";
+            this.light_3.Size = new System.Drawing.Size(100, 60);
+            this.light_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.light_3.TabIndex = 10;
+            this.light_3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.light_2);
+            this.Controls.Add(this.light_1);
+            this.Controls.Add(this.light_4);
+            this.Controls.Add(this.light_3);
+            this.Controls.Add(this.light_6);
+            this.Controls.Add(this.light_7);
+            this.Controls.Add(this.light_5);
+            this.Controls.Add(this.pb_Garage);
+            this.Controls.Add(this.btnCloseG);
+            this.Controls.Add(this.btnOpenG);
+            this.Controls.Add(this.btnCloseDoor);
+            this.Controls.Add(this.btnOpenDoor);
+            this.Controls.Add(this.pb_Door);
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Door)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_Garage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light_3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_Door;
+        private System.Windows.Forms.Button btnOpenDoor;
+        private System.Windows.Forms.Button btnCloseDoor;
+        private System.Windows.Forms.Button btnOpenG;
+        private System.Windows.Forms.Button btnCloseG;
+        private System.Windows.Forms.PictureBox pb_Garage;
+        private System.Windows.Forms.PictureBox light_5;
+        private System.Windows.Forms.PictureBox light_7;
+        private System.Windows.Forms.PictureBox light_6;
+        private System.Windows.Forms.PictureBox light_4;
+        private System.Windows.Forms.PictureBox light_1;
+        private System.Windows.Forms.PictureBox light_2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox light_3;
+    }
+}
+
---
+++ PlantillTallerArduino/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PlantillTallerArduino.Properties;
+using System.Threading;
+
+namespace PlantillTallerArduino
+{
+    public partial class Form1 : Form
+    {
+        List<Image> sprites = new List<Image>();
+        List<PictureBox> lights = new List<PictureBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getLights();
+            getSprites();
+            this.pb_Door.BackgroundImage = Resources.PuertaCerrada;
+            this.btnCloseDoor.Enabled = false;
+            this.pb_Garage.BackgroundImage = Resources.c1;
+            this.pb_Garage.BackgroundImageLayout = ImageLayout.Stretch;
+            this.pb_Door.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        void getLights()
+        {
+            lights.Add(light_1);
+            lights.Add(light_2);
+            lights.Add(light_3);
+            lights.Add(light_4);
+            lights.Add(light_5);
+            lights.Add(light_6);
+            lights.Add(light_7);
+
+        }
+        void getSprites()
+        {
+            sprites.Add(Resources.c1);
+            sprites.Add(Resources.c2);
+            sprites.Add(Resources.c3);
+            sprites.Add(Resources.c4);
+            sprites.Add(Resources.c5);
+            sprites.Add(Resources.c6);
+            sprites.Add(Resources.c7);
+            sprites.Add(Resources.c8);
+            sprites.Add(Resources.c9);
+            sprites.Add(Resources.c10);
+            sprites.Add(Resources.c11);
+            sprites.Add(Resources.c12);
+            sprites.Add(Resources.c13);
+            sprites.Add(Resources.c14);
+            sprites.Add(Resources.c15);
+            sprites.Add(Resources.c17);
+            sprites.Add(Resources.C18);
+
+        }
+        
+        void switchLight(bool status, int light )
+        {
+
+            try
+            {
+                if (light<lights.Count)
+                {
+                    lights[light].BackgroundImageLayout = ImageLayout.Stretch;
+                    if (status)
+                    {
+                        lights[light].BackgroundImage = Resources.lightOn;
+                    }
+                    else
+                    {
+                        lights[light].BackgroundImage = Resources.lightOff;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void btnOpenDoor_Click(object sender, EventArgs e)
+        {
+            door(true);
+        }
+
+        private void door(bool status)
+        {
+            this.pb_Door.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            if (status)
+            {
+                this.pb_Door.BackgroundImage = Resources.PuertaAbierta;
+                this.btnOpenDoor.Enabled = false;
+                this.btnCloseDoor.Enabled = true;
+
+
+            }
+            else
+            {
+                this.pb_Door.BackgroundImage = Resources.PuertaCerrada;
+                this.btnOpenDoor.Enabled = true;
+                this.btnCloseDoor.Enabled = false;
+            }
+
+        }
+
+        private void btnCloseDoor_Click(object sender, EventArgs e)
+        {
+            door(false);
+        }
+
+        public void garage(object obj)
+        {
+            try
+            {
+                var status = (bool)obj;
+            
+                this.pb_Garage.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                if (status)
+                {
+                 
+                    for (int i = 0; i < 17; i++)
+                    {
+                        Thread.Sleep(100);
+
+                        pb_Garage.BackgroundImage = sprites[i];
+                    }
+                }
+                else
+                {
+                    for (int i = 16; i >= 0; i--)
+                    {
+                        pb_Garage.BackgroundImage = sprites[i];
+                        Thread.Sleep(100);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            
+        }
+        private void btnOpenG_Click(object sender, EventArgs e)
+        {
+            btnCloseG.Enabled = true;
+            btnOpenG.Enabled = false;
+            new Thread(new ParameterizedThreadStart(garage)).Start(true);
+        }
+        
+        private void btnCloseG_Click(object sender, EventArgs e)
+        {
+            btnCloseG.Enabled = false;
+            btnOpenG.Enabled = true;
+            new Thread(new ParameterizedThreadStart(garage)).Start(false);
+        }
+
+        private void light_6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < lights.Count; i++)
+                {
+                    switchLight(true, i);
+                    Thread.Sleep(50);
+                }
+            })).Start();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new Thread(new ThreadStart(() =>
+            {
+                for (int i = 0; i < lights.Count; i++)
+                {
+                    switchLight(false, i);
+                    Thread.Sleep(50);
+                }
+            })).Start();
+        }
+    }
+}
